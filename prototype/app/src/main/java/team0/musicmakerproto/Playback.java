5f74aac7f1ec527a245c3e47ee81748aa9db4c5c
ShekharK <karki.shekhar@ymail.com>
2017-11-13 19:39:48 -0600

    skip functionalities

--- prototype/app/src/main/java/team0/musicmakerproto/Playback.java
+++ prototype/app/src/main/java/team0/musicmakerproto/Playback.java
@@ -1,6 +1,7 @@
 package team0.musicmakerproto;
 
 import android.media.MediaPlayer;
+import android.widget.Toast;
 
 /**
  * Class: Playback
@@ -9,6 +10,7 @@ import android.media.MediaPlayer;
 
 public class Playback {
     MediaPlayer currentSong;
+    int pause;
     Playlist playlist; //Keep track of the playlist from which the current song is stored.
     boolean isPlaying; //Keep track of if a song is currently played or paused.
 
@@ -19,7 +21,15 @@ public class Playback {
 
     public void play()
     {
+        if (currentSong == null) {
+           // currentSong = MediaPlayer.create(this, R.raw.weeknd);
+            currentSong.start();
 
+    }
+        else if(!currentSong.isPlaying()){
+            currentSong.seekTo(pause);
+            currentSong.start();
+        }
     }
 
     public void play(int id)
@@ -27,14 +37,15 @@ public class Playback {
 
     }
 
-    public void skipForward()
+    private static void skipForward()
     {
+    Playback.skipForward();
 
     }
 
-    public void skipBackward()
+    private static void skipBackward()
     {
-
+        Playback.skipBackward();
     }
 
 }
