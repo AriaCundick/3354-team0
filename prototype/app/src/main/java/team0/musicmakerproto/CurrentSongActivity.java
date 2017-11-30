88bd5539b9e8ea1666757c76862099c8cd396767
gabriellengo <gabrielletngo@gmail.com>
2017-11-30 00:04:46 -0600

    onClickListeners
    added onClickListners for shuffle/repeat buttons on current song activity to indicate if button has been activated

--- prototype/app/src/main/java/team0/musicmakerproto/CurrentSongActivity.java
+++ prototype/app/src/main/java/team0/musicmakerproto/CurrentSongActivity.java
@@ -2,14 +2,43 @@ package team0.musicmakerproto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.graphics.Color;
 
 public class CurrentSongActivity extends AppCompatActivity {
+
+    // button variables
+    ImageButton repeat;
+    ImageButton shuffle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_song);
+
+        repeat = (ImageButton) findViewById(R.id.repeat);
+        shuffle = (ImageButton) findViewById(R.id.shuffle);
+
+        // repeat button onClickListener
+        repeat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // click buton code here
+                repeat.setColorFilter(Color.parseColor("#6eb7a7"));
+            }
+        });
+
+        // shuffle button onClickListener
+        shuffle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // click buton code here
+                shuffle.setColorFilter(Color.parseColor("#d19e4f"));
+            }
+        });
     }
 
-    // open notes activity from page
+
+
 }
